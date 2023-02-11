b21c48809e8ec49a7d7b73161b97303f0a2a5857
Tobias Bieniek <tobias@bieniek.cloud>
2023-02-11 17:03:44 +0100

    Add `release-it` setup

---
+++ .release-it.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+
+module.exports = {
+  plugins: {
+    '@release-it-plugins/lerna-changelog': {
+      infile: 'CHANGELOG.md',
+      launchEditor: true,
+    },
+  },
+  git: {
+    commitMessage: 'v${version}',
+    tagName: 'v${version}',
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+    tokenRef: 'GITHUB_AUTH',
+  },
+  npm: {
+    publish: false,
+  },
+};
